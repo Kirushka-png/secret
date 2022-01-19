264817b2b61167266614993eac8c6a121a2028b5
Kosarev Kirill <66671677+Kirushka-png@users.noreply.github.com>
2022-01-19 17:17:24 +0300

    Add files via upload

---
+++ bootstrapLab/cart.js
@@ -0,0 +1,331 @@
+let userData = [
+    {
+        img: "img/item_5.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 415,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_5.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 415,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_5.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 415,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_5.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 430,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_5.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 430,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_5.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 430,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_6.png",
+        name: "Сливочно-черничное с цельными ягодами черники",
+        price: 375,
+        discr: "Сливочно-черничное с цельными ягодами черники"
+    },
+    {
+        img: "img/item_6.png",
+        name: "Сливочно-черничное с цельными ягодами черники",
+        price: 375,
+        discr: "Сливочно-черничное с цельными ягодами черники"
+    },
+    {
+        img: "img/item_6.png",
+        name: "Сливочно-черничное с цельными ягодами черники",
+        price: 375,
+        discr: "Сливочно-черничное с цельными ягодами черники"
+    },
+    {
+        img: "img/item_6.png",
+        name: "Сливочно-черничное с цельными ягодами черники",
+        price: 540,
+        discr: "Сливочно-черничное с цельными ягодами черники"
+    },
+    {
+        img: "img/item_6.png",
+        name: "Сливочно-черничное с цельными ягодами черники",
+        price: 540,
+        discr: "Сливочно-черничное с цельными ягодами черники"
+    },
+    {
+        img: "img/item_6.png",
+        name: "Сливочно-черничное с цельными ягодами черники",
+        price: 540,
+        discr: "Сливочно-черничное с цельными ягодами черники"
+    },
+    {
+        img: "img/item_7.png",
+        name: "Сливочное с апельсиновым джемом и цитрусовой стружкой",
+        price: 480,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_7.png",
+        name: "Сливочное с апельсиновым джемом и цитрусовой стружкой",
+        price: 480,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_7.png",
+        name: "Сливочное с апельсиновым джемом и цитрусовой стружкой",
+        price: 480,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_7.png",
+        name: "Сливочное с апельсиновым джемом и цитрусовой стружкой",
+        price: 400,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_7.png",
+        name: "Сливочное с апельсиновым джемом и цитрусовой стружкой",
+        price: 400,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_7.png",
+        name: "Сливочное с апельсиновым джемом и цитрусовой стружкой",
+        price: 400,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_8.png",
+        name: "Сливочно-кофейное с кусочками шоколада",
+        price: 415,
+        discr: "Сливочно-кофейное с кусочками шоколада"
+    },
+    {
+        img: "img/item_8.png",
+        name: "Сливочно-кофейное с кусочками шоколада",
+        price: 415,
+        discr: "Сливочно-кофейное с кусочками шоколада"
+    },
+    {
+        img: "img/item_8.png",
+        name: "Сливочно-кофейное с кусочками шоколада",
+        price: 415,
+        discr: "Сливочно-кофейное с кусочками шоколада"
+    },
+    {
+        img: "img/item_8.png",
+        name: "Сливочно-кофейное с кусочками шоколада",
+        price: 505,
+        discr: "Сливочно-кофейное с кусочками шоколада"
+    },
+    {
+        img: "img/item_8.png",
+        name: "Сливочно-кофейное с кусочками шоколада",
+        price: 505,
+        discr: "Сливочно-кофейное с кусочками шоколада"
+    },
+    {
+        img: "img/item_8.png",
+        name: "Сливочно-кофейное с кусочками шоколада",
+        price: 505,
+        discr: "Сливочно-кофейное с кусочками шоколада"
+    },
+    {
+        img: "img/item_9.png",
+        name: "Сливочное крем-брюле с карамельной подливкой",
+        price: 480,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_9.png",
+        name: "Сливочное крем-брюле с карамельной подливкой",
+        price: 480,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_9.png",
+        name: "Сливочное крем-брюле с карамельной подливкой",
+        price: 480,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_9.png",
+        name: "Сливочное крем-брюле с карамельной подливкой",
+        price: 400,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_9.png",
+        name: "Сливочное крем-брюле с карамельной подливкой",
+        price: 400,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_9.png",
+        name: "Сливочное крем-брюле с карамельной подливкой",
+        price: 400,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_10.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 415,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_10.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 415,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_10.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 415,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_10.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 505,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_10.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 505,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    },
+    {
+        img: "img/item_10.png",
+        name: "Сливочно-клубничное с присыпкой из белого шоколада",
+        price: 505,
+        discr: "Сливочно-клубничное с присыпкой из белого шоколада"
+    }
+]
+const updateAll = () => {
+    updateCounter()
+    fillCart()
+}
+//localStorage.clear()
+const addItem = (item) => {
+    if (item != undefined) {
+        let count = document.getElementById("itemsCount").value
+        if (localStorage.msgs) {
+            let tempArr = JSON.parse(localStorage.msgs)
+            tempArr.push({ ...item, count: count })
+            localStorage.setItem('msgs', JSON.stringify(tempArr))
+        }
+        else {
+            localStorage.setItem('msgs', JSON.stringify([{ ...item, count: count }]))
+        }
+        updateCounter();
+    }
+}
+const updateCounter = () => {
+    let basket = `<a href="cart.html" class="rounded-pill text-decoration-none text-white">${localStorage.msgs && JSON.parse(localStorage.msgs).length != 0 ? JSON.parse(localStorage.msgs).length : "Пусто"}</a>`
+    document.getElementById("header_form").innerHTML = basket
+}
+
+const removeOne = (idx) => {
+    if (localStorage.msgs) {
+        let tempArr = JSON.parse(localStorage.msgs)
+        tempArr.forEach((item, i) => {
+            i == idx && item.count != 1 && item.count--
+        });
+        localStorage.setItem('msgs', JSON.stringify(tempArr))
+        updateAll()
+    }
+}
+const addOne = (idx) => {
+    if (localStorage.msgs) {
+        let tempArr = JSON.parse(localStorage.msgs)
+        tempArr.forEach((item, i) => {
+            i == idx && item.count++
+        });
+        localStorage.setItem('msgs', JSON.stringify(tempArr))
+        updateAll()
+    }
+}
+
+const RemoveThis = (idx) => {
+    if (localStorage.msgs) {
+        let tempArr = JSON.parse(localStorage.msgs)
+        tempArr.splice(idx, 1)
+        localStorage.setItem('msgs', JSON.stringify(tempArr))
+        updateAll()
+    }
+}
+const RemoveAll = () =>{
+    localStorage.setItem('msgs', JSON.stringify([]))
+    updateAll()
+
+}
+
+const fillCart = () => {
+    let cartItem = ''
+    if (localStorage.msgs && JSON.parse(localStorage.msgs).length != 0) {
+        let tempArr = JSON.parse(localStorage.msgs)
+        let sum = 0
+        tempArr.forEach((item, idx) => {
+            sum += item.count * item.price
+            cartItem += `<div class="col-12">
+                <div class="row">
+                    <div class="col-2">
+                        <img class="cartItem" src="${item.img}">
+                    </div>
+                    <div class="col-8 d-flex flex-column align-items-center justify-content-center text-white">
+                        <span>${item.name}</span>
+                        <span>${item.price + "₽/кг"}</span>
+                        <div class="d-flex gap-3">
+                            <button onclick="removeOne(${idx})" class="btn btn-primary rounded-pill d-flex justify-content-center align-items-center" style="width: 30px; height: 30px;">-</button>
+                            <span>${item.count}</span>
+                            <button onclick="addOne(${idx})" class="btn btn-primary rounded-pill d-flex justify-content-center align-items-center" style="width: 30px; height: 30px;">+</button>
+                        </div>
+                    </div>
+                    <div class="col-2 d-flex align-items-center justify-content-center">
+                        <button class="custombtn p-2" onclick="RemoveThis(${idx})">Удалить</button>
+                    </div>
+                </div>
+            </div>`
+        });
+        cartItem += `<div class="col-12">
+                <div class="row gap-3">
+                        <div class="col-12 d-flex flex-column align-items-center justify-content-center text-white">
+                            <span>ИТОГО: ${sum}₽</span>
+                        </div>
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                    <button class="custombtn h-100 d-flex align-items-center" onclick="RemoveAll()">
+                    <span class="hidden">${sum} <img style="width:20px; height:20px;" src="img/cart.png""></span>
+                    <img class="icon" style="width:20px; height:20px;" src="img/cart.png">Оформить заказ!
+                    </button>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+    else{
+        cartItem = `<div class="col-12">
+                <div class="row">
+                        <div class="col-12 d-flex flex-column align-items-center justify-content-center text-white">
+                            <span>Пусто, закажите чего-нибудь</span>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+    }
+    document.getElementById("cartcontainer").innerHTML = cartItem
+}
